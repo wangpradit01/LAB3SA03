1fae0c16698f768202ce748a82f7188ba033ec74
sananya <s6135512036@phuket.psu.ac.th>
2020-09-10 09:53:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World{2 +3} <br/>
+      Hello ("World")
+    </div>
+  );
+}
+
+export default App;
